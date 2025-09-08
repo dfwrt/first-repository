fd00d2ccc28453a5b1423e025a5068f486e30da3
dfwrt <dfwrt470@gmail.com>
2025-09-08 15:16:28 +0900

    Second commit

--- python1.py
+++ python1.py
@@ -1,9 +1,10 @@
-def print_hello():
-    print("Hello World!!")
+def add(x,y):
+    a = x+y
+    return a
 
 def print_ABC():
-    print("ABC")
+    print("abcdef")
 
 if __name__ == '__main__' :
     print_ABC()
-    print_hello()
+    print(add(1,4))
